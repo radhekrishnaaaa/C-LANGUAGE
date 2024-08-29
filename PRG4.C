d19253112bf88a065bdbde38d2b0141818c3c612
bhumi prajapati <r2075308@gmail.com>
2024-08-29 16:49:10 +0530

    second commit

---
+++ PRG4.C
@@ -0,0 +1,11 @@
+//prg4:comment
+#include<stdio.h>
+int main()
+{
+	printf("1.hello");
+	//printf("2.hello");
+	printf("3.hello");
+	/*printf("4.hello");
+	printf("5.hello");*/
+	return 0;
+}
---
+++ PRG7.C
@@ -0,0 +1,11 @@
+//prg7: area of square
+#include<stdio.h>
+int main()
+{
+	int l,area;
+	printf("\n enter lanth of squre");
+	scanf("%d",&l);
+	area=l*l;
+	printf("\n %d area is=%d",l,area);
+	return 0;
+}
